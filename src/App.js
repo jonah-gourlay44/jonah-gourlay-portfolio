2f80783c01efdd4ceb01b6791f7d272d0797d1f7
Jonah <jonah.gourlay44@gmail.com>
2023-11-27 23:46:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './Portfolio.css'
+import MenuDrawer from './MenuDrawer'
+import HomeIcon from '@mui/icons-material/Home'
+import ScienceIcon from '@mui/icons-material/Science'
+import WorkIcon from '@mui/icons-material/Work'
+import SchoolIcon from '@mui/icons-material/School'
+
+import HomePage from './HomePage'
+import SchoolPage from './SchoolPage'
+import WorkPage from './WorkPage'
+import ProjectPage from './ProjectPage'
+
+const theme = createTheme({
+  palette: {
+    grey: {
+      main: '#545353',
+    },
+  }
+})
 
 function App() {
+  const menu = 
+    <MenuDrawer items={[
+      {
+        page: 'Home',
+        icon: <HomeIcon />,
+        link: '/jonah-portfolio'
+      },
+      {
+        page: 'Coursework',
+        icon: <SchoolIcon />,
+        link: '/jonah-portfolio/coursework'
+      },
+      {
+        page: 'Personal Projects',
+        icon: <ScienceIcon />,
+        link: '/jonah-portfolio/projects'
+      },
+      {
+        page: 'Work Experience',
+        icon: <WorkIcon />,
+        link: '/jonah-portfolio/experience'
+      }
+    ]} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme} >
+        <Router>
+          <Routes>
+            <Route path='/jonah-portfolio' element={<HomePage menu={menu} />} />
+            <Route path='/jonah-portfolio/coursework' element={<SchoolPage menu={menu} />} />
+            <Route path='/jonah-portfolio/projects' element={<ProjectPage menu={menu} />} />
+            <Route path='/jonah-portfolio/experience' element={<WorkPage menu={menu} />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
---
+++ src/SchoolPage.js
@@ -0,0 +1,9 @@
+import Container from '@mui/material/Container'
+
+export default function SchoolPage({menu}) {
+    return (
+        <Container>
+            {menu}
+        </Container>
+    )
+}
